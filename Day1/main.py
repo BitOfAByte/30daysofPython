f40771af571f138d6060a758786453860f19317f
DaatUserName <hopetobylol@gmail.com>
2022-01-04 12:22:57 +0100

    First day of 30daysofpython

---
+++ Day1/main.py
@@ -0,0 +1,17 @@
+#Task 1
+
+a = int(input("Number 1: "))
+b = int(input("Number 2: "))
+print("The sum is: ", a+b)
+
+#Task 2
+s1 = "Hello, "
+s2 = "World! "
+print("String 1", s1)
+print("String 2", s2)
+print("Result ", s1+s2)
+
+#Task 3
+c = float(input("Number 1: "))
+d = float(input("Number 2: "))
+print("The sum is: ", c+d)
