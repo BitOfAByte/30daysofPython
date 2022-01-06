34f3b23d56ce9939fb899493f5f36a8507aed277
DaatUserName <hopetobylol@gmail.com>
2022-01-06 10:04:21 +0100

    Day 2

---
+++ Day2/main.py
@@ -0,0 +1,35 @@
+
+#Task 1
+#Taken from https://www.geeksforgeeks.org/python-program-for-compound-interest/
+#Formula to calculate compound interest annually is given by:
+#A = P(1 + R/100) t
+#Compound Interest = A – P
+#Where:
+#A is amount
+#P is principle amount
+#R is the rate and
+#T is the time span
+
+def compound_interest(principle, rate, time):
+    Amount = principle * (pow((1 + rate / 100), time))
+    CI = Amount - principle
+    print("Compound interest is", CI)
+compound_interest(2000, 5.25, 10)
+
+#Task 2
+def calcPercent(num,p):
+    num=int(input("Number 1: "))
+    p=float(input("Percent: "))
+    result = num * p;
+    print("Percentage is: ", result)
+
+calcPercent(10,10)
+
+#Task 3
+
+s = "Hello I'm toby and you?"
+first, *middle, last = s.split()
+
+print(first,middle,last)
+
+
