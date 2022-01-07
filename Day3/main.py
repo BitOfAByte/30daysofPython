af4c1f8d8276d1bbc5b2860422ac5899caa10e7e
DaatUserName <hopetobylol@gmail.com>
2022-01-07 13:00:30 +0100

    bruh

---
+++ Day3/main.py
@@ -0,0 +1,26 @@
+#Task 1
+
+def calcModolo(num,num2):
+    print(num % num2)
+
+calcModolo(5,100)
+def calcDevision(num, num2):
+    print(num / num2)
+calcDevision(10.5,5.2)
+
+def calExponent(num, num2):
+    print(pow(num,num2))
+
+
+calExponent(20,10)
+
+
+#Task 2
+
+print("\n\nTask 2")
+s = "Pooop"
+if s == "Toby":
+    print("Hello Toby")
+else:
+    name = input("What's your name? ")
+    print("Hello " , name)
